11309c7c4ef8ce880174944b7e38b07f13ca3da1
FreeSpiritIntellect <liberatedman32@gmail.com>
2025-10-26 16:01:35 +0800

    4

---
+++ CarLinkChannel/NewApi/TCUStreamBasedRequest.h
@@ -0,0 +1,16 @@
+//
+//  TCUStreamBasedRequest.h
+//  CarLinkChannel
+//
+//  Created by 刘润泽 on 2025/10/25.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TCUStreamBasedRequest : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ CarLinkChannel/NewApi/TCUStreamBasedRequest.m
@@ -0,0 +1,12 @@
+//
+//  TCUStreamBasedRequest.m
+//  CarLinkChannel
+//
+//  Created by 刘润泽 on 2025/10/25.
+//
+
+#import "TCUStreamBasedRequest.h"
+
+@implementation TCUStreamBasedRequest
+
+@end
